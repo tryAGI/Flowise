fd96bf2b45db4fbb9c743126f52c33e7e6d57ad9
Konstantin S. <havendv@gmail.com>
2026-03-19 19:49:04 +0400

    feat: Updated OpenAPI spec (#11)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Flowise/Generated/Flowise.AttachmentsClient.CreateAttachment.g.cs
+++ src/libs/Flowise/Generated/Flowise.AttachmentsClient.CreateAttachment.g.cs
@@ -85,9 +85,18 @@ namespace Flowise
             __httpRequestContent.Add(
                 content: new global::System.Net.Http.StringContent($"{chatId}"),
                 name: "\"chatId\"");
-            __httpRequestContent.Add(
-                content: new global::System.Net.Http.StringContent($"[{string.Join(",", global::System.Linq.Enumerable.Select(request.Files, x => x))}]"),
-                name: "\"files\"");
+            for (var __iFiles = 0; __iFiles < request.Files.Count; __iFiles++)
+            {
+                var __contentFiles = new global::System.Net.Http.ByteArrayContent(request.Files[__iFiles]);
+                __httpRequestContent.Add(
+                    content: __contentFiles,
+                    name: "\"files\"",
+                    fileName: $"\"file{__iFiles}.bin\"");
+                if (__contentFiles.Headers.ContentDisposition != null)
+                {
+                    __contentFiles.Headers.ContentDisposition.FileNameStar = null;
+                }
+            }
             if (request.Base64 != default)
             {
 
